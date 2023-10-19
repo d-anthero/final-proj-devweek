fd501c68514f18ad85ba141a15199c84f9dc1b9d
d-anthero <d.anthero09@gmail.com>
2023-10-19 14:08:54 -0300

    update

---
+++ src/main/java/me/finalprojdevweek/FinalProjDevweekApplication.java
@@ -0,0 +1,13 @@
+package me.finalprojdevweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjDevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjDevweekApplication.class, args);
+	}
+
+}
